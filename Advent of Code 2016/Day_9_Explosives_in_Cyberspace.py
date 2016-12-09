35d84a9c49c50182e760b86aea9049aeb98d0766
schnappischnap <benjaminianwatson@gmail.com>
2016-12-09 11:43:52 +0000

    Completed day 9.

---
+++ Advent of Code 2016/Day_9_Explosives_in_Cyberspace.py
@@ -0,0 +1,28 @@
+# Parts 1 and 2
+def decompressed_length(input, part):
+    total = 0
+    i = 0
+    while i < len(input):
+        if input[i] == '(':
+            i += 1
+            marker = ""
+            while input[i] != ')':
+                marker += input[i]
+                i += 1
+            character_count, repeat_amount = [int(x) for x in marker.split('x')]
+            if part == 1:
+                total += repeat_amount * character_count
+            else:
+                total += repeat_amount * decompressed_length(input[(i + 1) : (i + character_count + 1)], 2)
+            i += character_count
+        else:
+            total += 1
+        i += 1
+    return total
+
+
+if __name__ == "__main__":
+    with open("day_9_input.txt") as f:
+        input = f.read()
+        print "Part 1 answer: " + str(decompressed_length(input, 1))
+        print "Part 2 answer: " + str(decompressed_length(input, 2))
